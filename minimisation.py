465043e26a6a37fbe04c37ab7f6a6e770ac86e99
Elena <e.queirolo@vu.nl>
2020-04-24 19:07:10 -0400

    not very successful

--- minimisation.py
+++ minimisation.py
@@ -8,10 +8,9 @@ import numpy as np
 from scipy import optimize
 import matplotlib.pyplot as plt
 from math import log
-from hill_model import HillComponent, ToggleSwitch, SaddleNode, full_newton, unit_phase_condition, \
-    diff_unit_phase_condition, find_root
+from hill_model import HillComponent, ToggleSwitch, SaddleNode, find_root
 
-
+"""
 def distance_saddle(parameters):
     decay_loc = parameters[0:2]
     par1 = parameters[2:5]
@@ -42,10 +41,7 @@ def distance_saddle(parameters):
 
     distance = np.abs(P[0] - Q[0])
     return -distance
-
-
-def distance_saddle_with_constraint():
-    return 1
+"""
 
 
 # create the Toggle Switch problem with only one variable: the first decay
@@ -60,28 +56,35 @@ def SaddleProblem_loc(gamma1):
     return SaddleNode(ToggleFixed(gamma1), unit_phase_condition, diff_unit_phase_condition)
 
 
+def SN_call_temp(SNinstance, parameter, u0):
+    """Temporary SaddleNode call outside the main class definition"""
+
+    return find_root(lambda u: SNinstance.zero_map(u, parameter), lambda u: SNinstance.diff_zero_map(u, parameter), u0, diagnose=True)
+
+
 def findSaddle(interval, N=300):
     Hill_coef = 5
-    gamma_grid = interval[0] + range(N) / (interval[1] - interval[0])
+    decay2 = float(2)
+    p1 = np.array([1, 5, 3], dtype=float)
+    p2 = np.array([1, 6, 3], dtype=float)
+
+
+    gamma_grid = np.linspace(interval[0], interval[1], N)
+    saddle_all = np.array([])
     for gamma in gamma_grid:
         equilibria = ToggleFixed(gamma).find_equilibria(Hill_coef, 10)
         SN_loc = SaddleProblem_loc(gamma)
-        v0 = np.random.uniform(0, 10, 2)
-        saddle = full_newton(SN_loc.zero_map, SN_loc.diff_zero_map,
-                             np.concatenate((equilibria[:, 1], v0, np.array(Hill_coef)), axis=None))
+        parameter = np.concatenate((gamma, decay2, p1, p2), axis=None)
+        saddle = SN_call_temp(SN_loc, parameter, equilibria[:, 1])
+        saddle_all = np.concatenate(saddle_all, saddle, axis=1)
 
-    return gamma_grid
-
-
-# set some parameters to test using MATLAB toggle switch for ground truth
-"""decay2 = float(2)
-p1 = np.array([1, 5, 3], dtype=float)
-p2 = np.array([1, 6, 3], dtype=float)
-x0 = np.array([4, 3]) """
-# first_parameters = np.concatenate((decay, p1, p2), axis=None)
+    # select only the good saddle_all
+    saddle_good = np.unique(np.round(saddle_all, 7), axis=0)
+    return saddle_good
 
 
-# distance_saddle(first_parameters)
+
+findSaddle([0,30])
 
 # optimize.minimize(distance_saddle, first_parameters, method='nelder-mead')
 # should work with at least one fixed parameter.

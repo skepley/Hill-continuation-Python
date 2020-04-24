1c882a6e0228ffd28a5b0212d801805ee1dd7017
Elena <e.queirolo@vu.nl>
2020-04-24 13:12:10 -0400

    palying around

--- minimisation.py
+++ minimisation.py
@@ -9,52 +9,37 @@ from scipy import optimize
 import matplotlib.pyplot as plt
 from math import log
 from hill_model import HillComponent, ToggleSwitch, SaddleNode, full_newton, unit_phase_condition, \
-    diff_unit_phase_condition
-
-# def find_equilibria(self, parameter, gridDensity, uniqueRootDigits=7):
-#     """
-#     Return equilibria for the Hill Model by uniformly sampling for initial conditions and iterating a Newton variant.
-#     INPUT:
-#         parameter - A vector of all variable parameters to use for evaluating the root finding algorithm
-#         gridDensity - (int) density to sample in each dimension.
-#         uniqueRootDigits - (int) Number of digits to use for distinguishing between floats."""
-#
-#     # Include root finding method as vararg
-#     def F(x):
-#         """Fix parameter values in the zero finding map"""
-#         return self.__call__(x, parameter)
-#
-#     def DF(x):
-#         """Fix parameter values in the zero finding map derivative"""
-#         return self.dx(x, parameter)
-#
-#     # build a grid of initial data for Newton algorithm
-#     evalGrid = np.meshgrid(*[np.linspace(*f_i.eq_interval(), num=gridDensity) for f_i in self.coordinates])
-#     X = np.row_stack([G_i.flatten() for G_i in evalGrid])
-#     solns = list(filter(lambda root: root.success,
-#                         [find_root(F, DF, X[:, j])
-#                          for j in range(X.shape[1])]))  # return equilibria which converged
-#     equilibria = np.column_stack([root.x for root in solns])  # extra equilibria as vectors in R^n
-#     equilibria = np.unique(np.round(equilibria, uniqueRootDigits), axis=1)  # remove duplicates
-#     return np.column_stack([find_root(F, DF, equilibria[:, j]).x for j in
-#                             range(np.shape(equilibria)[1])])  # Iterate Newton again to regain lost digits
+    diff_unit_phase_condition, find_root
 
 
-def distance_saddle(decay_loc, par1, par2):
+def distance_saddle(parameters):
+    decay_loc = parameters[0:2]
+    par1 = parameters[2:5]
+    par2 = parameters[5:]
+
     TS_model = ToggleSwitch(decay_loc, [par1, par2])
     Hill_coef = 4.1
     equilibria = TS_model.find_equilibria(Hill_coef, 10)
+    # 10 is the grid density
 
     SN_model = SaddleNode(TS_model, unit_phase_condition, diff_unit_phase_condition)
 
-    x0 = equilibria[:, 1]
     v0 = np.array([1, -.7])
-    u0 = np.concatenate((x0, v0, np.array(Hill_coef)), axis=None)
 
-    SN_solution = full_newton(SN_model.zero_map, SN_model.diff_zero_map, u0)
-    # do many many tries to actually find two saddle nodes
+    solns = list([full_newton(SN_model.zero_map, SN_model.diff_zero_map,
+                              np.concatenate((equilibria[:, j], v0, np.array(Hill_coef)), axis=None))
+                  for j in range(np.shape(equilibria)[1])])
 
-    P, Q = search_saddles()
+    saddles = np.unique(np.round(solns, 7), axis=1)  # remove duplicates
+
+    if saddles.shape[1] > 1:
+        return 0
+    if saddles.shape[1] < 1:
+        return 0
+
+    P = saddles[:, 1]
+    Q = saddles[:, 0]
+
     distance = np.abs(P[0] - Q[0])
     return -distance
 
@@ -63,37 +48,42 @@ def distance_saddle_with_constraint():
     return 1
 
 
+# create the Toggle Switch problem with only one variable: the first decay
+def ToggleFixed(gamma1):
+    decay2 = float(2)
+    p1 = np.array([1, 5, 3], dtype=float)
+    p2 = np.array([1, 6, 3], dtype=float)
+    return ToggleSwitch([gamma1, decay2], [p1, p2])
+
+
+def SaddleProblem_loc(gamma1):
+    return SaddleNode(ToggleFixed(gamma1), unit_phase_condition, diff_unit_phase_condition)
+
+
+def findSaddle(interval, N=300):
+    Hill_coef = 5
+    gamma_grid = interval[0] + range(N) / (interval[1] - interval[0])
+    for gamma in gamma_grid:
+        equilibria = ToggleFixed(gamma).find_equilibria(Hill_coef, 10)
+        SN_loc = SaddleProblem_loc(gamma)
+        v0 = np.random.uniform(0, 10, 2)
+        saddle = full_newton(SN_loc.zero_map, SN_loc.diff_zero_map,
+                             np.concatenate((equilibria[:, 1], v0, np.array(Hill_coef)), axis=None))
+
+    return gamma_grid
+
+
 # set some parameters to test using MATLAB toggle switch for ground truth
-decay = np.array([1, 1], dtype=float)
+"""decay2 = float(2)
 p1 = np.array([1, 5, 3], dtype=float)
 p2 = np.array([1, 6, 3], dtype=float)
-x0 = np.array([4, 3])
+x0 = np.array([4, 3]) """
+# first_parameters = np.concatenate((decay, p1, p2), axis=None)
 
-f = ToggleSwitch(decay, [p1, p2])
-n = 4.1
-n0 = np.array([n])
 
-SN = SaddleNode(f, unit_phase_condition, diff_unit_phase_condition)
+# distance_saddle(first_parameters)
 
-eq = f.find_equilibria(n, 10)
-
-# v0 = np.array([1, 1])
-v0 = np.array([1, -.7])
-# v0 = eq[:, 0] - eq[:, 1]
-# v0 = v0 / np.linalg.norm(v0)
-x0 = eq[:, 1]
-u0 = np.concatenate((x0, v0, np.array(n)), axis=None)
-
-# print(SN.zero_map(u0))
-# print('\n')
-# print(SN.diff_zero_map(u0))
-
-# sol = find_root(SN.zero_map, SN.diff_zero_map, u0)
-# sol = optimize.root(SN.zero_map, u0, method='hybr')  # set root finding algorithm
-# SNsol = sol.x
-SNsol = full_newton(SN.zero_map, SN.diff_zero_map, u0)
-
-optimize.minimize(distance_saddle(), first_parameters)
+# optimize.minimize(distance_saddle, first_parameters, method='nelder-mead')
 # should work with at least one fixed parameter.
 
 # if we want to leave all parameters free, we also want to add a request about norm of the vector of parameters = 1
